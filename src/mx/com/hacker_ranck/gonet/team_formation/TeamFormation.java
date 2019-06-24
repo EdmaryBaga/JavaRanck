88374434fed075e5950e2b34d34e382471a4e88a
Edith.bautista <edith.bautista@gonet.us>
2019-06-24 13:07:37 -0500

    se recuperan y ordenan los elementos que resive TeamFormation

--- src/mx/com/hacker_ranck/gonet/team_formation/TeamFormation.java
+++ src/mx/com/hacker_ranck/gonet/team_formation/TeamFormation.java
@@ -11,38 +11,71 @@ public class TeamFormation {
 	
 	private static final Scanner scanner = new Scanner(System.in);
 	
+	
 	public static void main(String[] args) throws IOException {
-
-		ArrayList <ArrayList> equipos= new ArrayList();
-		int t = scanner.nextInt();
-		for(int i=0; i<t;i++) {
-			equipos.add(new ArrayList());
+		ArrayList<ArrayList<Integer>> listaEquipos = new ArrayList<>();
+		
+		listaEquipos.addAll(recogeDatos());
+		for(int p=0; p<listaEquipos.size(); p++) {
+			listaEquipos.get(p).sort(new Comparator<Integer>() {
+			    @Override
+			    public int compare(Integer o1, Integer o2) {
+			        return o1.compareTo(o2);
+			    }
+			});
+			//verificaTamEquipoX(listaEquipos.get(0));
+				
+			for(int x:listaEquipos.get(p) ) {
+				System.out.print(x + " ");
+			}
+			System.out.println(" ");
 		}
-
-		 for (int r = 0; r < t; r++) {
-    		int m = scanner.nextInt();
-        	equipos.get(r).add(m);
-        	scanner.nextLine();
-        }
-        
-		 
-        for(int o=0; o<equipos.size();o++) {
-        	ArrayList<Integer> aux = new ArrayList<Integer>();
-        	aux.addAll(equipos.get(o));
-        	
-        	for(int x: aux) {
-        		System.out.print(x);
-        	}
-        	System.out.println(" ");
-        	
-        }
-        
-        scanner.close();
+		
 	}
-
-	private static int Equipos(int s) {
-		//System.out.println(s.charAt(0));
-		return s;
+	
+	private static int verificaTamEquipoX(int[] equipo) {
+		//ArrayList<Integer> le= Arrays.asList(equipo);	
+		
+		Map<Integer, HashSet<Integer>> lista = new HashMap<>();
+		lista.put(1,new HashSet<Integer>());
+		HashSet<String> eq = new HashSet<String>();
+		int contSubEquipos=1;
+		for(int p=0; p<equipo.length; p++) {
+			if(!(lista.get(1).contains(equipo[p]))) {
+				lista.get(1).add(equipo[p]);
+			}
+			else {
+				contSubEquipos+=1;
+				lista.put(contSubEquipos,new HashSet<Integer>());
+				lista.get(contSubEquipos).add(equipo[p]);
+			}
+		}
+		for(int p= 0; p<lista.get(1).size(); p++) {
+			System.out.println(lista.get(p));
+		}
+		
+		return 0;
+	}
+	
+	public static ArrayList<ArrayList<Integer>> recogeDatos() {
+		
+		int numEquipos = scanner.nextInt();
+		ArrayList<ArrayList<Integer>> listEquipos = new ArrayList<ArrayList<Integer>>();
+		for(int i=0; i<numEquipos; i++) {
+			int tamEquipo = scanner.nextInt();
+			ArrayList<Integer> equipoX = new ArrayList<Integer>();
+			for (int t=0; t<tamEquipo; t++) {
+				if (scanner.hasNextInt()) {
+					 do {
+						 equipoX.add(scanner.nextInt());
+						 break;
+						 
+					 }while(scanner.hasNextInt());
+				 }
+			}
+			listEquipos.add(equipoX);
+		}
+		return listEquipos;
 	}
 
 }
