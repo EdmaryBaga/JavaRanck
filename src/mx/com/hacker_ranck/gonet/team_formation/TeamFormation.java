b172972475027a41f4d8c5716827da7c2658d450
Edith.bautista <edith.bautista@gonet.us>
2019-06-24 16:02:00 -0500

    Se realizan los subgrupos del ejemplo 1 correctamente

--- src/mx/com/hacker_ranck/gonet/team_formation/TeamFormation.java
+++ src/mx/com/hacker_ranck/gonet/team_formation/TeamFormation.java
@@ -23,36 +23,73 @@ public class TeamFormation {
 			        return o1.compareTo(o2);
 			    }
 			});
-			//verificaTamEquipoX(listaEquipos.get(0));
-				
-			for(int x:listaEquipos.get(p) ) {
-				System.out.print(x + " ");
-			}
-			System.out.println(" ");
+			verificaTamEquipoX(listaEquipos.get(p));			
 		}
 		
 	}
 	
-	private static int verificaTamEquipoX(int[] equipo) {
+	private static int verificaTamEquipoX(ArrayList<Integer> equipo) {
+		//IMPRIMIMOS LO QUE SE RESIVE
+		for(int x:equipo) {
+			System.out.print(x + " ");
+		}
+		Map<Integer, HashSet<Integer>> lista = new HashMap<>();
+		lista.put(1,new HashSet<Integer>());
+		int key=1;
+		for(int p=0; p<equipo.size(); p++) {
+			
+			if(lista.get(key).isEmpty()) {
+				lista.get(key).add(equipo.get(p));
+			}else if(p==equipo.size()-1) {
+				lista.get(key).add(equipo.get(p));
+			}
+			else if(!(equipo.get(p)+1==equipo.get(p+1)) || equipo.get(p).equals(equipo.get(p+1))) {
+				//verificar si algun set contiene ese valor
+				lista.get(key).add(equipo.get(p));
+				key+=1;
+				lista.put(key,new HashSet<Integer>());
+				lista.get(key).add(equipo.get(p+1));
+				p+=1;
+			}
+			else {
+				lista.get(key).add(equipo.get(p));
+			}
+		}
+		
+		for(int o=1; o<=lista.size(); o++) {
+			//lista.get(o).forEach(action);
+			System.out.println(" imprimiendo subgrupos");
+			Iterator<Integer> i = lista.get(o).iterator(); 
+			while (i.hasNext()) {
+	            System.out.print(i.next()+" "); 
+	    } 
+			System.out.println(" ");
+		}
+		
+		System.out.println(" \n -------- * --------");
+		
+		
+		
+		
 		//ArrayList<Integer> le= Arrays.asList(equipo);	
 		
-		Map<Integer, HashSet<Integer>> lista = new HashMap<>();
+		/*Map<Integer, HashSet<Integer>> lista = new HashMap<>();
 		lista.put(1,new HashSet<Integer>());
 		HashSet<String> eq = new HashSet<String>();
 		int contSubEquipos=1;
-		for(int p=0; p<equipo.length; p++) {
-			if(!(lista.get(1).contains(equipo[p]))) {
-				lista.get(1).add(equipo[p]);
+		for(int p=0; p<equipo.size(); p++) {
+			if(!(lista.get(1).contains(equipo.get(p)))) {
+				lista.get(1).add(equipo.get(p));
 			}
 			else {
 				contSubEquipos+=1;
 				lista.put(contSubEquipos,new HashSet<Integer>());
-				lista.get(contSubEquipos).add(equipo[p]);
+				lista.get(contSubEquipos).add(equipo.get(p));
 			}
 		}
 		for(int p= 0; p<lista.get(1).size(); p++) {
 			System.out.println(lista.get(p));
-		}
+		}*/
 		
 		return 0;
 	}
