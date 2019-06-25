0e3e9ab02fd3df08f163022fe3d4f00c08333d24
Edith.bautista <edith.bautista@gonet.us>
2019-06-25 11:22:54 -0500

    Se agrego la solucion de el ejercicio de JavaRegex

--- src/mx/com/hacker_ranck/gonet/exam2/regex/JavaRagex.java
+++ src/mx/com/hacker_ranck/gonet/exam2/regex/JavaRagex.java
@@ -1,5 +1,28 @@
 package mx.com.hacker_ranck.gonet.exam2.regex;
 
-public class JavaRagex {
+import java.util.Scanner;
 
+class Solution{
+
+    public static void main(String[] args){
+        @SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            new MyRegex();
+			System.out.println(IP.matches(MyRegex.pattern));
+        }
+
+    }
 }
+
+class MyRegex{
+    public static final String pattern ="^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+}
+/**
+ * Expresiones regulares:
+ * ^ indica el principio del string
+ * $ final del string
+ * [] Definicion de un conjunto valido
+ * ? 0 o 1 ocurrencias
+ * */
