108c825c02a9dd5d9bc94cdb1fae0260a3e01991
Edith.bautista <edith.bautista@gonet.us>
2019-06-25 11:23:33 -0500

    Se resolvio el problema del examen 2 llamado StringToken

--- src/mx/com/hacker_ranck/gonet/exam2/string_token/StringToken.java
+++ src/mx/com/hacker_ranck/gonet/exam2/string_token/StringToken.java
@@ -2,6 +2,28 @@ package mx.com.hacker_ranck.gonet.exam2.string_token;
 import java.io.*;
 import java.util.*;
 
+/*
+ * Solucion con todos los casos
+ * 
+ * public class Solution {
+
+     public static void main(String[] args)
+   {
+       Scanner scan = new Scanner(System.in);
+       String s=scan.nextLine();
+       s.trim();
+       StringTokenizer st = new StringTokenizer(s,("[_\\@!?.', ]"));
+       int x = st.countTokens();
+       System.out.println(x);
+       while(st.hasMoreTokens()){
+           System.out.println(st.nextToken());
+       }
+       scan.close();
+   }
+}
+
+ * */
+
 class Solution {
 
     public static void main(String[] args) {
